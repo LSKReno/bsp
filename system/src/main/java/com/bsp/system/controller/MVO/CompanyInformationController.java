8a4c654348e5f917364a867244b5388d11ca55a0
87574 <875747176@qq.com>
2020-07-03 14:13:35 +0800

    - finish CompanyInformationController - update ManManufacturerService

--- system/src/main/java/com/bsp/system/controller/MVO/CompanyInformationController.java
+++ system/src/main/java/com/bsp/system/controller/MVO/CompanyInformationController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @RestController
 @RequestMapping("/CompanyInformationController")
@@ -18,9 +20,42 @@ public class CompanyInformationController {
     @Resource
     private ManManufacturerService manManufacturerService;
     @PostMapping("/getCompanyInfo")
-    public ResponseDto getCompanyInfo(@RequestBody PageDto pageDto){
+    public ResponseDto getCompanyInfo(@RequestBody ManManufacturerDto manManufacturerDto){
         ResponseDto responseDto = new ResponseDto();
-        manManufacturerService.list(pageDto);
+        ManManufacturer manManufacturer = manManufacturerService.selectByPrimaryKey(manManufacturerDto);
+        if (manManufacturer != null){
+            responseDto.setSuccess(false);
+        }else{
+            responseDto.setSuccess(true);
+            responseDto.setContent(manManufacturer);
+        }
+        return responseDto;
+    }
+    @PostMapping("/saveCompanyInfo")
+    public ResponseDto saveCompanyInfo(@RequestBody ManManufacturerDto manManufacturerDto) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        ResponseDto responseDto = new ResponseDto();
+//        ManManufacturer manManufacturer = new ManManufacturer();
+//        manManufacturer.setCallCnt(manManufacturerDto.getCallCnt());
+//        manManufacturer.setCreatedBy(manManufacturerDto.getCreatedBy());
+//        manManufacturer.setCreationDate(simpleDateFormat.parse(manManufacturerDto.getCreationDate()));
+//        manManufacturer.setDeleted(manManufacturerDto.getDeleted() == 1);
+//        manManufacturer.setDescription(manManufacturerDto.getDescription());
+//        manManufacturer.setGmcReportType(manManufacturerDto.getGmcReportType());
+//        manManufacturer.setGmcReportUrl(manManufacturerDto.getGmcReportUrl());
+//        manManufacturer.setLastUpdateBy(manManufacturerDto.getLastUpdateBy());
+//        manManufacturer.setLastUpdateDate(simpleDateFormat.parse(manManufacturerDto.getLastUpdateDate()));
+//        manManufacturer.setStsCd(manManufacturerDto.getStsCd());
+//        manManufacturer.setRemark(manManufacturerDto.getRemark());
+//        manManufacturer.setNameEn(manManufacturerDto.getNameEn());
+//        manManufacturer.setNameCn(manManufacturerDto.getNameCn());
+//        manManufacturer.setManId(manManufacturerDto.getManId());
+        int result = manManufacturerService.save(manManufacturerDto);
+        if(result == 0){
+            responseDto.setSuccess(false);
+        }else{
+            responseDto.setSuccess(true);
+        }
         return responseDto;
     }
 }
