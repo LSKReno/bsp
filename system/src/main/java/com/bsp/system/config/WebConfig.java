b6e6089c1ade43ac0d91fcf704d754aeb5acbe60
LSKReno <18236198930@163.com>
2020-07-06 12:34:02 +0800

    refactor: system module's WebConfig class

--- system/src/main/java/com/bsp/system/config/WebConfig.java
+++ system/src/main/java/com/bsp/system/config/WebConfig.java
@@ -2,14 +2,17 @@ package com.bsp.system.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+
 import javax.annotation.Resource;
+
 /**
  * Created by LSK.Reno on 2020/7/6 8:30.
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Resource
-    private TokenInterceptor tokenInterceptor ;
+    private TokenInterceptor tokenInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
