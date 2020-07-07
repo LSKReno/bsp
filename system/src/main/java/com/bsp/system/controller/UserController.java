c603c9146c064c7b8b7bd852950e8e9a91e9e3cb
LSKReno <18236198930@163.com>
2020-07-07 08:59:16 +0800

    fix: signup and login validation

--- system/src/main/java/com/bsp/system/controller/UserController.java
+++ system/src/main/java/com/bsp/system/controller/UserController.java
@@ -36,20 +36,8 @@ public class UserController {
     @PostMapping("/login")
     public ResponseDto login(@RequestBody SysUserDto sysUserDto, HttpServletRequest request) {
         // 保存校验
-        ValidatorUtil.require(sysUserDto.getUserId(), "主键");
         ValidatorUtil.length(sysUserDto.getUsername(), "用户名，唯一", 1, 255);
         ValidatorUtil.length(sysUserDto.getPassword(), "密码", 1, 255);
-        ValidatorUtil.length(sysUserDto.getName(), "昵称（可重复）", 1, 255);
-        ValidatorUtil.length(sysUserDto.getRights(), "权限", 1, 255);
-        ValidatorUtil.length(sysUserDto.getRoleId(), "角色ID", 1, 100);
-        ValidatorUtil.length(sysUserDto.getLastLogin(), "上次登录时间", 1, 255);
-        ValidatorUtil.length(sysUserDto.getIp(), "登录IP", 1, 100);
-        ValidatorUtil.length(sysUserDto.getStatus(), "状态", 1, 32);
-        ValidatorUtil.length(sysUserDto.getBz(), "备注", 1, 255);
-        ValidatorUtil.length(sysUserDto.getSkin(), "皮肤样式", 1, 100);
-        ValidatorUtil.length(sysUserDto.getEmail(), "邮箱", 1, 32);
-        ValidatorUtil.length(sysUserDto.getNumber(), "固话号码", 1, 100);
-        ValidatorUtil.length(sysUserDto.getPhone(), "手机", 1, 32);
         LOG.info("用户登录开始");
         JSONObject json = new JSONObject();
 
@@ -78,10 +66,8 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseDto signup(@RequestBody SysUserDto sysUserDto) {
         // 保存校验
-        ValidatorUtil.require(sysUserDto.getUserId(), "主键");
         ValidatorUtil.length(sysUserDto.getUsername(), "用户名，唯一", 1, 255);
         ValidatorUtil.length(sysUserDto.getPassword(), "密码", 1, 255);
-        ValidatorUtil.length(sysUserDto.getName(), "昵称（可重复）", 1, 255);
 
         LOG.info("用户注册开始");
         JSONObject json = new JSONObject();
