87ba87876baf92a03018f2b3ce4ea56132fe97aa
LSKReno <18236198930@163.com>
2020-07-07 09:01:54 +0800

    feat: add '/signup' to token intercepter

--- system/src/main/java/com/bsp/system/config/TokenInterceptor.java
+++ system/src/main/java/com/bsp/system/config/TokenInterceptor.java
@@ -25,7 +25,7 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
                              Object handler) throws SignatureException {
         /** 地址过滤 */
         String uri = request.getRequestURI();
-        if (uri.contains("/login")) {
+        if (uri.contains("/login") ||uri.contains("/signup")) {
             return true;
         }
 
