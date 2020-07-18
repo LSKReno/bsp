3c87731b484a8fd665ac0f229561f1df4d28e343
87574 <875747176@qq.com>
2020-07-18 12:13:41 +0800

    - finish tracking number

---
+++ server/src/main/java/com/bsp/server/util/RedisUtil.java
@@ -0,0 +1,31 @@
+package com.bsp.server.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public long incr(String key, long delta) {
+        if (delta < 0) {
+            throw new RuntimeException("delta should bigger than 0");
+        }
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
